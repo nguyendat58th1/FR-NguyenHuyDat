3457eec0de22680acd817c82220688afc187b522
Huy Dat <huydat3008@gmail.com>
2021-12-31 13:45:10 +0700

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.Write("Input name : ");
+            name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}");
+            Console.WriteLine("Welcome to NTQ !!");
+        }
+    }
+}
