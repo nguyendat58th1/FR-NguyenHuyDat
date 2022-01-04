8197107e29ec377bfe20744b9008ec9d936cee6a
Huy Dat <huydat3008@gmail.com>
2022-01-04 11:18:42 +0700

    Value and Reference type , Struct

---
+++ Struct/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Struct
+{
+    public struct Rectangle
+    {
+        public double Width;
+        public double Height;
+
+        public static void Input(out Rectangle re)
+        {
+            Console.Write("Height :");
+            re.Height = Double.Parse(Console.ReadLine());
+            Console.Write("Width : ");
+            re.Width = Double.Parse(Console.ReadLine());
+        
+        }
+        public static double Area(Rectangle re)
+        {
+            return re.Height * re.Width;
+        }
+
+        public static double Perimeter(Rectangle re)
+        {
+            return re.Height + re.Width;
+        }
+
+        public static Calculator GetAreaPerimeter(Rectangle re)
+        {
+            Calculator ca = new Calculator();
+            ca.Area = Area(re);
+            ca.Perimeter = Perimeter(re);
+            return ca;
+        }
+        static void Main(string[] args)
+        {
+            //Rectangle re = new Rectangle();
+            //Input(out re);
+            //Console.WriteLine($"Area : {GetAreaPerimeter(re).Area}");
+            //Console.WriteLine($"Perimeter : {GetAreaPerimeter(re).Perimeter}");
+            Rectangle[] arrRe = new Rectangle[5];
+            for(int i =0; i< arrRe.Length;i++)
+            {
+                Console.WriteLine($"Input rectangle nunber {i}");
+                Input( out arrRe[i]);
+                Console.WriteLine($"Area rectangle nunber {i} : {GetAreaPerimeter(arrRe[i]).Area}");
+                Console.WriteLine($"Perimeter rectangle nunber {i} : {GetAreaPerimeter(arrRe[i]).Perimeter}");
+                Console.WriteLine();
+            }    
+        }
+    }
+    public struct Calculator
+    {
+        public double Area;
+        public double Perimeter;
+    }
+}
---
+++ ValueAndReferenceType/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ValueAndReferenceType
+{
+    class Program
+    {
+        class ValueTypeNReferenceType
+        {
+            public static void Main(string[] args)
+            {
+                string name = "Peter";
+                int number = 10;
+                Console.WriteLine(name);
+                Console.WriteLine(number);
+                ChangelessName(name);
+                ChangelessNumber(number);
+                Console.WriteLine(name);
+                Console.WriteLine(number);
+                ChangeName(out name);
+                ChangeNumber(ref number);
+                Console.WriteLine(name);
+                Console.WriteLine(number);
+
+            }
+
+            static string ChangelessName(string name)
+            {
+                name = "Eva";
+                return name;
+            }
+            static int ChangelessNumber(int number)
+            {
+                return number++;
+            }
+            static string ChangeName(out string name)
+            {
+                name = "Steve";
+                return name;
+            }
+            static int ChangeNumber(ref int number)
+            {
+                return number++;
+            }
+        }
+    }
+}
