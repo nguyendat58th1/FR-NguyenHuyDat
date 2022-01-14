728f32fbe38d4e3586873d9f6d604c8fc1faf155
Huy Dat <huydat3008@gmail.com>
2022-01-14 11:22:31 +0700

    Add Delegate event

---
+++ DelegateEvent/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Text;
+
+namespace DelegateEvent
+{
+
+    public class MyEventArgs : EventArgs
+    {
+        public MyEventArgs(string data)
+        {
+            this.data = data;
+        }
+        // Lưu dữ liệu gửi đi từ publisher
+        private string data;
+
+        public string Data
+        {
+            get { return data; }
+        }
+    }
+
+    class Family
+    {
+        public event EventHandler sendMessage;
+        public void Send()
+        {
+            sendMessage?.Invoke(this,new MyEventArgs("Thông báo em bé đã chào đời !"));
+        }
+    }
+    class Friend
+    {
+        public string Name { get; set; }
+        public Friend(string name)
+        {
+            this.Name = name;
+        }
+
+        public void Sub(Family flm)
+        {
+            flm.sendMessage += Notify;
+        }
+        public void Notify(object sender, EventArgs e)
+        {
+            MyEventArgs mea = (MyEventArgs)e;
+            Console.WriteLine($"{this.Name } : {mea.Data}");
+        }
+
+    }
+    class Neighbor
+    {
+        public void Sub(Family flm)
+        {
+            flm.sendMessage += Notify;
+        }
+        public void Notify(object sender, EventArgs e)
+        {
+            MyEventArgs mea = (MyEventArgs)e;
+            Console.WriteLine($"Hàng xóm: {mea.Data}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Family flm = new Family();
+            Friend fr1 = new Friend("Nam");
+            Neighbor neighbor = new Neighbor();
+            neighbor.Sub(flm);
+            fr1.Sub(flm);
+            flm.Send();
+        }
+    }
+}
---
+++ IOFile/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace IOFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            File.WriteAllText("abc.txt", "Hello");
+            string a = File.ReadAllText("abc.txt");
+            Console.WriteLine(a);
+        }
+    }
+}
